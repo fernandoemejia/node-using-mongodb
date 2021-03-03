ca57d918b4b23286d92130da9a5c918d06c78bd9
Fernando Mejia <fernandoemejia@yahoo.com>
2021-03-02 21:47:25 -0500

    Complete Module2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose'
 
 const app = express();
 const PORT = 4000;
+//mongoose connection
+mongoose.Promise=global.Promise;
 
-
-mongoose.connect('mongodb://localhost:27017/productsdb', {useNewUrlParser: true, useUnifiedTopolog:true});
+mongoose.connect('mongodb://localhost:27017/productsdb', {useNewUrlParser: true, useUnifiedTopology:true});
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
